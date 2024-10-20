c0e522530a7722b7be986563b50c912913480d6f
sakinanazarali <98000420+sakinanazarali@users.noreply.github.com>
2024-10-19 20:03:18 -0700

    added dummy code for git practice

---
+++ dummy_code.py
@@ -0,0 +1,12 @@
+import numpy as np
+import random
+
+# Generate 5 random integers between 1 and 100
+random_numbers = [random.randint(1, 100) for _ in range(5)]
+
+# Print the random numbers
+print("Here are 5 random numbers:")
+for num in random_numbers:
+    print(num)
+
+# I think we need real comments to this 
